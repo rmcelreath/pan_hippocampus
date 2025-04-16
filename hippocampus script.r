22090c305ddf50cc09d5fefa8b23bdce26093f74
Richard McElreath <richard_mcelreath@eva.mpg.de>
2025-04-16 08:33:33 +0200

    prior predictive update

--- hippocampus script.r
+++ hippocampus script.r
@@ -30,6 +30,13 @@ dat$hr <- d[,'RIGHT hippocampus volume(mm3)']
 dat$logHL <- log(dat$hl)
 dat$logHR <- log(dat$hr)
 
+# anterior/posterior right and left
+dat$logAHR <- log(d[,'aHPC_Right_volume(mm3)'])
+dat$logAHL <- log(d[,'aHPC_Left_volume(mm3)'])
+
+dat$logPHR <- log(d[,'pHPC_Right_volume(mm3)'])
+dat$logPHL <- log(d[,'pHPC_Left_volume(mm3)'])
+
 ######################################################
 # visualize sample
 
@@ -71,7 +78,7 @@ precis(m0,2)
 post <- extract.samples(m0)
 diff <- post$a[,2]-post$a[,1]
 quantile(diff)
-dens(post$a[,2]-post$a[,1])
+dens(post$a[,2]-post$a[,1],show.zero=TRUE)
 
 # mass below zero
 sum(diff<0)/length(diff)
@@ -79,26 +86,29 @@ sum(diff<0)/length(diff)
 ######################################################
 # baseline model with left/right instead of average
 
-m1 <- ulam(
+# without covariance, just as scaffold to develop the covariance version
+m1_dev <- ulam(
     alist(
         logHL ~ normal(mu,sigma),
         logHR ~ normal(mu,sigma),
         mu <- a[R] + bB*logB + bA*A,
-        a[R] ~ normal(0,5),
+        a[R] ~ normal(-5,2),
         bB ~ normal(1,1),
         bA ~ normal(1,1),
         sigma ~ exponential(1)
     ), data=dat , chains=4 , cores=4 )
 
 # covariance version
+# priors here from prior predictive logic in doc
 m1 <- ulam(
     alist(
         c(logHL,logHR) ~ multi_normal(c(muL,muR),Rho,Sigma),
-        muL <- a[R] + bB*logB,
-        muR <- a[R] + bB*logB + delta,
-        a[R] ~ normal(0,2),
+        muR <- muL + delta,
+        muL <- a[R] + (bB + g*A)*logB,
+        a[R] ~ normal(-5.7,2),
         delta ~ normal(0,0.5),
         bB ~ normal(1,0.5),
+        g ~ normal(0,0.5),
         Rho ~ lkj_corr(4),
         vector[2]:Sigma <<- rep_vector(sigma,2),
         sigma ~ exponential(1)
@@ -116,6 +126,55 @@ dens(post$a[,2]-post$a[,1],show.zero=TRUE,xlab="contrast [wild-captive]")
 mbz <- sum(diff<0)/length(diff)
 text(0.15,2,round(1-mbz,3))
 
+# prior predictive for covariance model
+
+N <- 50
+Hcaptive <- matrix(NA,N,2)
+Hwild <- matrix(NA,N,2)
+logB <- rep(NA,N)
+A <- rep(NA,N)
+for ( i in 1:N ) {
+    a <- rnorm(2,-5.7,2)
+    delta <- rnorm(1,0,0.1)
+    bB <- rnorm(1,1,0.25)
+    Rho <- rlkjcorr(1,K=2,eta=4)
+    sigma <- rnorm(1,0.1,0.1)
+    idx <- sample(1:16,size=1)
+    logB[i] <- dat$logB[idx]
+    A[i] <- dat$A[idx]
+    mucaptive <- a[1] + bB*logB[i]
+    Hcaptive[i,] <- rmvnorm2(1,Mu=c(mucaptive,mucaptive+delta),Rho=Rho,sigma=rep(sigma,2))
+    muwild <- a[2] + bB*logB[i]
+    Hwild[i,] <- rmvnorm2(1,Mu=c(muwild,muwild+delta),Rho=Rho,sigma=rep(sigma,2))
+}
+
+# blank2()
+plot( (logB) , (Hcaptive[,1]) , xlab="log Brain" , ylab="log Hippocampus" )
+points( (logB) , (Hwild[,1]) , col=2 )
+
+######################################################
+# add anterior/posterior volumes
+
+m2 <- ulam(
+    alist(
+        c(logHL,logHR,logAHL,logAHR,logPHL,logPHR) ~ multi_normal(c(muL,muR,muAL,muAR,muPL,muPR),Rho,Sigma),
+        muR <- muL + delta,
+        muL <- a[R] + bB*logB,
+        muAR <- muAL + delta,
+        muAL <- aA[R] + bB*logB,
+        muPR <- muPL + delta,
+        muPL <- aP[R] + bB*logB,
+        a[R] ~ normal(-5.7,2),
+        aA[R] ~ normal(-6,2),
+        aP[R] ~ normal(-6,2),
+        delta ~ normal(0,0.5),
+        bB ~ normal(1,0.1),
+        Rho ~ lkj_corr(4),
+        Sigma ~ exponential(1)
+    ), data=dat , chains=8 , cores=8 , refresh=1000 )
+
+precis(m2,3)
+
 ######################################################
 # model hippocampus as age-residence-specific proportion of brain B
 # Y = p[A,R]*B   =>   log(Y) = log(B) + log(p[A,R])
@@ -126,15 +185,15 @@ m2 <- ulam(
     alist(
         # hippocampus model
         c(logHL,logHR) ~ multi_normal(c(muL,muR),Rho,Sigma),
+        muR <- muL + delta,
         muL <- a[R] + p*A + logB,
-        muR <- a[R] + p*A + logB + delta,
         p ~ normal(0,0.5),
         delta ~ normal(0,0.5),
         # brain size model
         logB ~ normal(nu,tau),
         nu <- b + log(1-exp(-phi*A)),
         # priors
-        a[R] ~ normal(0,10),
+        a[R] ~ normal(-5,5),
         Rho ~ lkj_corr(4),
         vector[2]:Sigma <<- rep_vector(sigma,2),
         sigma ~ exponential(1),
